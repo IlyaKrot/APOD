ad0fc14b930d5804884eda104ba1507e1a1d1c4f
Ilya <ilya008800@yandex.ru>
2020-10-30 16:17:39 +0300

    create

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let title = document.querySelector('.apod-title__title');
+let img = document.querySelector('.apod-img__img');
+let explanation = document.querySelector('.apod-desc__explanation');
+let date = document.querySelector('.apod-desc__date');
+let copyright = document.querySelector('.apod-desc__copyright');
+let data = {}
+
+async function getData() {
+  await fetch('https://api.nasa.gov/planetary/apod?api_key=CgcS8t8ZP8lhqcDx8beL0rHakJMhLMcyTqXfKD8E')
+  .then(res => res.json())
+  .then(json => {
+    data = json
+  });
+  console.log(data)
+  title.textContent = data.title.toUpperCase()
+  img.src = data.hdurl
+  img.alt = data.title
+  explanation.textContent = data.explanation
+  date.textContent = data.date
+  copyright.textContent = data.copyright;
+}
+
+getData()
+console.log(img.src)
